18917c7c3bde655dcf27f6a5c14796a2fdef5a4d
yapni <yapni8@gmail.com>
2017-10-25 20:46:34 +0800

    updated maps stuff for attractions + busroutes

---
+++ app/constants/AttractionList.js
@@ -0,0 +1,378 @@
+const ATTRACTIONS = [
+  {
+  "operatingHours": {
+    "Sat": {
+      "Opening": "1000",
+      "Closing": "1700"
+    },
+    "Tue": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sun": {
+      "Opening": "1000",
+      "Closing": "1600"
+    },
+    "Mon": {
+
+    },
+    "Wed": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Fri": {
+      "Opening": "1000",
+      "Closing": "1700"
+    },
+    "Thur": {
+      "Opening": "1000",
+      "Closing": "1900"
+    }
+  },
+  "name": "Indian Heritage Centre",
+  "updatedAt": "2017-10-24 04:36:26.0",
+  "specialHours": null,
+  "latitude": 1.3056813,
+  "url": "www.indianheritage.org.sg",
+  "description": "The Indian Heritage Centre traces the history of the Indian and South Asian communities in the Southeast Asian Region. Located in the Little India Heritage District, the centre houses two permanent galleries, small scale museum facilities, a museum shop as well as programming and activity spaces.",
+  "redemptionDetails": "Last redemption at one hour before closing",
+  "longitude": 103.850072,
+  "merchantID": "896685",
+  "id": "cj952msrk056m01486zqqgga4",
+  "addressString": "5 Campbell Lane, Singapore 209924",
+  "createdAt": "2017-10-24 03:48:32.0",
+  "directions": "By MRT – Alight at Bugis Station (Exit B). Take a 10-minute walk towards Sultan Mosque By SIA Hop-on Bus – Alight at Stop 5 (Red). By SIA Hop-on Bus – Alight at Stop 5 (Red)",
+  "inclusion": "General Admission (Redeem at ticketing counter)"
+}, {
+  "operatingHours": {
+    "Sat": {
+      "Opening": "1000",
+      "Closing": "1800"
+    },
+    "Tue": {
+      "Opening": "1000",
+      "Closing": "1800"
+    },
+    "Sun": {
+      "Opening": "1000",
+      "Closing": "1800"
+    },
+    "Mon": {
+
+    },
+    "Wed": {
+      "Opening": "1000",
+      "Closing": "1800"
+    },
+    "Fri": {
+      "Opening": "1000",
+      "Closing": "1800"
+    },
+    "Thur": {
+      "Opening": "1000",
+      "Closing": "1800"
+    }
+  },
+  "name": "Malay Heritage Centre",
+  "updatedAt": "2017-10-24 04:36:44.0",
+  "specialHours": null,
+  "latitude": 1.3031694,
+  "url": "www.malayheritage.org.sg",
+  "description": "The Malay heritage centre showcases the history culture and contributions of the Malay community within the context of Singapore’s history and multi-cultural society. It houses a collection of artefacts that focuses on Kampong Gelam’s history as a bustling port town integral to the development of the Malay community.",
+  "redemptionDetails": "Last redemption at one hour before closing",
+  "longitude": 103.857571,
+  "merchantID": "295072",
+  "id": "cj953tnhv05eo0148219zjff8",
+  "addressString": "85 Sultan Gate, Singapore 198501",
+  "createdAt": "2017-10-24 04:21:52.0",
+  "directions": "By MRT – Alight at Bugis Station (Exit B). Take a 10-minute walk towards Sultan Mosque. By SIA Hop-on Bus – Alight at Stop 5 (Red)",
+  "inclusion": "General Admission (Redeem at ticketing counter)"
+}, {
+  "operatingHours": {
+    "Sat": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Tue": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Sun": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Mon": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Wed": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Fri": {
+      "Opening": "1915",
+      "Closing": "0000"
+    },
+    "Thur": {
+      "Opening": "1915",
+      "Closing": "0000"
+    }
+  },
+  "name": "Night Safari",
+  "updatedAt": "2017-10-24 04:36:51.0",
+  "specialHours": null,
+  "latitude": 1.4049843,
+  "url": "www.nightsafari.com.sg",
+  "description": "Get an up close and personal encounter with over 2,500 nocturnal wildlife when you take a guided tram tour.  Explore six geographical zones including the Himalayan Foothills, the Asian Riverine Forest and the Indian Subcontinent. Conclude your advernture with the Creatures of the Night Show, which features an amazing line-up of animals.",
+  "redemptionDetails": "last redemption at one and a half hours before closing.",
+  "longitude": 103.7883602,
+  "merchantID": "565539",
+  "id": "cj953x61q05gc0148xd8f3nx1",
+  "addressString": "80 Mandai Lake Road, Singapore 729826",
+  "createdAt": "2017-10-24 04:24:36.0",
+  "directions": "By MRT and Bus – Alight at Ang Mo Kio Station (Exit C). Board bus 138 from bus interchange or alight at Choa Chu Kang Station (Exit C) and board bus 92 from the bus interchange",
+  "inclusion": "General Admission (Redeem at ticketing counter 1 or 2)"
+}, {
+  "operatingHours": {
+    "Thu": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Sat": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Tue": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Sun": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Mon": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Wed": {
+      "Opening": "0830",
+      "Closing": "1800"
+    },
+    "Fri": {
+      "Opening": "0830",
+      "Closing": "1800"
+    }
+  },
+  "name": "Singapore Zoo",
+  "updatedAt": "2017-10-24 04:37:11.0",
+  "specialHours": null,
+  "latitude": 1.4049843,
+  "url": "www.zoo.com.sg",
+  "description": "Singapore Zoo’s world famous “Open Concept” offers the opportunity to experience and be inspired by more than 2,800 animals from over 300 species. Come witness the wonders at this award winning, world’s best rainforest zoo.",
+  "redemptionDetails": "Last redemption at one hour before closing",
+  "longitude": 103.7883602,
+  "merchantID": "670267",
+  "id": "cj95489wc05id0148h5hewyzz",
+  "addressString": "80 Mandai Lake Road, Singapore 729826",
+  "createdAt": "2017-10-24 04:33:14.0",
+  "directions": "By MRT and Bus – Alight at Ang Mo Kio Station (Exit C). Board bus 138 from bus interchange or alight at Choat Chu Kang Station (Exit C) and board bus 92 from the bus interchange",
+  "inclusion": "General Admission (Tram ride not included, Redeem at ticketing counter 1 or 2)"
+}, {
+  "operatingHours": {
+    "Thu": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sat": {
+      "Opening": "1000",
+      "Closing": "2200"
+    },
+    "Tue": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sun": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Mon": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Wed": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Fri": {
+      "Opening": "1000",
+      "Closing": "2200"
+    }
+  },
+  "name": "National Gallery Singapore",
+  "updatedAt": "2017-10-24 04:45:35.0",
+  "specialHours": {
+    "25122017": {
+      "Reason": "Christmas",
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "24122017": {
+      "Reason": "Christmas Eve",
+      "Opening": "1000",
+      "Closing": "2200"
+    }
+  },
+  "latitude": 1.2902926,
+  "url": "nationalgallery.sg",
+  "description": "The National Gallery Singapore has been beautifully restored and transformed from two national monuments into the latest visual arts venue in Singapore. The Gallery will engage and inspire all with its largest public collection of modern Singapore and Southeast Asian Art.",
+  "redemptionDetails": "Last redemption at one hour before closing.",
+  "longitude": 103.8515068,
+  "merchantID": "184949",
+  "id": "cj954g3d905k40148eu2d1917",
+  "addressString": "1 Saint Andrew's Road, Singapore 178957",
+  "createdAt": "2017-10-24 04:39:19.0",
+  "directions": "By MRT and Bus – Alight at City Hall Station (Exit B). Walk along North Bridge Road to Parliament Place. By SIA Hop-on Bus – Alight at Stop 8 (Red), Stop 5 (Yellow), Stop 14 (Brown)",
+  "inclusion": "General Admission (Redeem at ticketing counter)"
+}, {
+  "operatingHours": {
+    "Thu": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sat": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Tue": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sun": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Mon": {
+
+    },
+    "Wed": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Fri": {
+      "Opening": "1000",
+      "Closing": "1900"
+    }
+  },
+  "name": "Lee Kong Chian Natural History Museum",
+  "updatedAt": "2017-10-24 04:52:10.0",
+  "specialHours": {
+    "25122017": {
+      "Reason": "Christmas",
+      "Opening": "1000",
+      "Closing": "1900"
+    }
+  },
+  "latitude": 1.3014053,
+  "url": "lkcnhm.nus.edu.sg",
+  "description": "Get acquainted with the incredible world of biodiversity at Singapore’s fist and only natural history museum! Experience the biologically diverse ecosystem of Southeast Asia through our specimens and interactive displays at our two galleries. Be awed by our three real and gigantic dinosaurs and learn more about our Singapore Whale.",
+  "redemptionDetails": "Last redemption at two hours before closing.",
+  "longitude": 103.7714563,
+  "merchantID": "221255",
+  "id": "cj954qhqt05m30148fyo7yruq",
+  "addressString": "National University of Singapore, 2 Conservatory Drive, Singapore 117377",
+  "createdAt": "2017-10-24 04:47:24.0",
+  "directions": "By MRT and Bus – Alight at Clementi Station. Board bus 96 from the bus interchange and alight at NUS Raffles Hall.",
+  "inclusion": "General Admission (Redeem at ticketing counter)"
+}, {
+  "operatingHours": {
+    "Thu": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sat": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Tue": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Sun": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Mon": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Wed": {
+      "Opening": "1000",
+      "Closing": "1900"
+    },
+    "Fri": {
+      "Opening": "1000",
+      "Closing": "1900"
+    }
+  },
+  "name": "Jurong Bird Park",
+  "updatedAt": "2017-10-24 04:57:36.0",
+  "specialHours": null,
+  "latitude": 1.3180714,
+  "url": "www.birdpark.com.sg",
+  "description": "See some 5,000 birds across 400 species fly, swim and roam freely in their naturalistic habitats, including 4 large free-flight aviaries at Asia’s largest bird paradise. Come up close with these feathered residents and be tickled by their show-stopping antics.",
+  "redemptionDetails": "Last redemption at one hour before closing.",
+  "longitude": 103.7046289,
+  "merchantID": "676407",
+  "id": "cj954zw6v05od0148oixkwd5l",
+  "addressString": "2 Jurong Hill, Singapore 628925",
+  "createdAt": "2017-10-24 04:54:42.0",
+  "directions": "By MRT and Bus – Alight at Boon Lay Station (Exit D). Board bus 194 from the bus interchange ",
+  "inclusion": "General Admission (Tram ride not included, Redeem at ticketing counter 5)"
+}, {
+  "operatingHours": {
+    "Thu": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Sat": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Tue": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Sun": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Mon": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Wed": {
+      "Opening": "0900",
+      "Closing": "2300"
+    },
+    "Fri": {
+      "Opening": "0900",
+      "Closing": "2300"
+    }
+  },
+  "name": "Singapore River Cruise",
+  "updatedAt": "2017-10-24 05:02:29.0",
+  "specialHours": null,
+  "latitude": 1.290668,
+  "url": "www.rivercruise.com.sg",
+  "description": "Embark on a scenic boat ride along the Singapore River and be in for a treat as the cruise sails past illustrious areas, such as Clarke Quay, Boat Quay and Marina Bay. Look out for the iconic Marina Bay Sands, Merlion Park and Esplanade – Theatres on the Bay",
+  "redemptionDetails": "Last redemption at one hour before closing.",
+  "longitude": 103.8467605,
+  "merchantID": "071194",
+  "id": "cj9558rkt05pf01484nzz5q4q",
+  "addressString": "Blk 3e River Valley Road, Singapore 179024",
+  "createdAt": "2017-10-24 05:01:36.0",
+  "directions": "By MRT and Bus – Alight at Clarke Quay Station (Exit E). Walk towards Block D Clarke Quay. By SIA Hop-on Bus – Alight at Stop 13 (Brown), Stop 6 (Yellow). Boarding and Alighting Point – Clarke Quay Jetty (near TCC)",
+  "inclusion": "Boat Ride with commentaries (Redeem at Clarke Quay Jetty ticketing counter)"
+}];
+
+export default ATTRACTIONS;
