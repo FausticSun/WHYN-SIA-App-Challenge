a8d599943ceb6001778222d9dd7291065a73ad3a
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2017-10-27 02:14:52 +0800

    update collapsible card to pop from botttom

--- app/components/CollapsibleCard.js
+++ app/components/CollapsibleCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Image, TouchableHighlight, Animated, Dimensions } from 'react-native';
-import { Card, Icon, CardItem, Text, Spinner } from 'native-base';
+import { StyleSheet, View, Image, TouchableHighlight, Animated, Dimensions, ScrollView } from 'react-native';
+import { Card, Icon, CardItem, Text, Spinner, Container, Content } from 'native-base';
 const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
 
@@ -9,100 +9,117 @@ export default class CollapsibleCard extends React.Component{
     super(props);
     this.state = {
       expanded : false,
-      animation : new Animated.Value()
+      animation : new Animated.Value(),
     };
   }
 
-    toggle(){
-      let initialValue = this.state.expanded ?
-        this.state.maxHeight + this.state.minHeight : this.state.minHeight;
-      let finalValue = this.state.expanded ?
-       this.state.minHeight : this.state.maxHeight + this.state.minHeight;
-
-      this.setState((prevState) => ({
-          expanded : !prevState.expanded
-      }));
-
-      this.state.animation.setValue(initialValue);
-      Animated.spring(this.state.animation, { toValue: finalValue }).start();
-    }
-
-    _setMaxHeight(event){
-
-      this.setState({ maxHeight : event.nativeEvent.layout.height,
-        animation : (new Animated.Value( this.state.minHeight)),  });
-    }
-
-    _setMinHeight(event){
-      this.setState({
-        minHeight : event.nativeEvent.layout.height,
-
-      });
-    }
-
-
-    _setToggleClosed() {
-    this.top.measure((ox, oy, width, height, px, py) => {
-        this.setState({
-            maxHeight: height
-        });
-    });
-    this.bot.measure((ox, oy, width, height, px, py) => {
-        this.setState({
-            minHeight: height
-        });
-        this.state.animation.setValue(height);
-    });
+  setTopHeight(event){
+    this.setState({
+       topHeight: event.nativeEvent.layout.height
+     });
   }
-    componentDidMount() {
-        setTimeout( this._setToggleClosed.bind(this) );
+
+  setBotHeight(event) {
+    this.setState({
+       botHeight: event.nativeEvent.layout.height
+     });
+
+     this.state.animation.setValue(-500);
+     Animated.spring(
+           this.state.animation,
+           {
+               toValue: -event.nativeEvent.layout.height
+           }
+       ).start();
+     this.setState({expanded: false});
+  }
+
+  toggle() {
+    if(this.state.expanded) {
+
+      this.state.animation.setValue(0);
+      Animated.spring(
+            this.state.animation,
+            {
+                toValue: -this.state.botHeight
+            }
+        ).start();
+
+    } else {
+      this.state.animation.setValue(-this.state.botHeight);
+      Animated.spring(
+            this.state.animation,
+            {
+                toValue: 0
+            }
+        ).start();
     }
 
-    render(){
+    this.setState((prev)=>({expanded: !prev.expanded}));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if(prevProps.title == this.props.title) {
+      return;
+    }
+
+  }
+
+
+
+    render() {
       const { title, description, imageURI } = this.props;
 
       return (
-        <Animated.View
-          style={[styles.container,{ height: this.state.animation}]}>
-          <Card>
-          <View
-            ref={c => this.top = c}
-            onLayout={this._setMinHeight.bind(this)}>
-          <TouchableHighlight
-              onPress={this.toggle.bind(this)}
-              underlayColor="#f1f1f1">
-            <CardItem style={{
-              height: 150,
-              width: '100%',
-              paddingTop: 0,
-              paddingRight: 0,
-              paddingLeft: 0,
-              paddingBottom: 0,
-            }}>
-                <Image
-                  style={{
-                    height: '100%',
-                    width: '100%'
-                  }}
-                  source={{uri: imageURI}}
-                >
-                  <Text style={{color: 'white'}}>{title}</Text>
-                </Image>
-            </CardItem>
-            </TouchableHighlight>
+        <Animated.View style={{position: 'absolute', bottom: this.state.animation}}>
 
+        <Card>
+          <View
+          ref={c => this.top = c}
+          onLayout={this.setTopHeight.bind(this)}>
+          <CardItem style={{
+            height: 150,
+            width: '100%',
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingLeft: 0,
+            paddingBottom: 0,
+          }}>
+          <TouchableHighlight
+          style={{
+            height: '100%',
+            width: '100%'
+          }}
+                        onPress={this.toggle.bind(this)}
+                        underlayColor="#f1f1f1">
+
+            <Image
+
+              style={{
+                height: '100%',
+                width: '100%'
+              }}
+              source={{uri: imageURI}}
+            >
+
+              <Text style={{color: 'white'}}>{title}</Text>
+            </Image>
+            </TouchableHighlight>
+          </CardItem>
           </View>
           <View
             ref={c => this.bot = c}
-            onLayout={this._setMaxHeight.bind(this)}>
+            onLayout={this.setBotHeight.bind(this)}>
             <CardItem>
               <Text>{description}</Text>
             </CardItem>
             <CardItem>
               <Text>Description of the year</Text>
             </CardItem>
-          </View>
+              </View>
           </Card>
+
         </Animated.View>
       );
     }
